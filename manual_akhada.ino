65c774d19c5404c76064c9e02da32ca2ac91da74
codedump00 <blazinasis@gmail.com>
2019-02-10 19:04:33 +0545

    rc battlebot

---
+++ manual_akhada.ino
@@ -0,0 +1,180 @@
+#define BRAKE 0
+#define CW    1
+#define CCW   2
+#define CS_THRESHOLD 15   // Definition of safety current (Check: "1.3 Monster Shield Example").
+
+//MOTOR 1
+#define MOTOR_A1_PIN 7
+#define MOTOR_B1_PIN 8
+
+//MOTOR 2
+#define MOTOR_A2_PIN 4
+#define MOTOR_B2_PIN 9
+
+#define PWM_MOTOR_1 5
+#define PWM_MOTOR_2 6
+
+#define CURRENT_SEN_1 A2
+#define CURRENT_SEN_2 A3
+
+#define EN_PIN_1 A0
+#define EN_PIN_2 A1
+
+#define MOTOR_1 0
+#define MOTOR_2 1
+
+//Channels to use
+#define CHANNEL1 10
+#define CHANNEL2 11
+#define CHANNEL3 12
+#define CHANNEL4 12
+
+
+//Channel pulse read parameters
+#define CHANNEL_NEUTRAL_LOW 1400
+#define CHANNEL_NEUTRAL_HIGH 1550
+#define CHANNEL_ONE_TWO_HIGH  1960
+#define CHANNEL_ONE_TWO_LOW  990
+
+short usSpeed = 150;  //default motor speed
+unsigned short usMotor_Status = BRAKE;
+//Array to store channel readings.
+int channelData[4];
+
+void setup()                         
+{
+  pinMode(MOTOR_A1_PIN, OUTPUT);
+  pinMode(MOTOR_B1_PIN, OUTPUT);
+
+  pinMode(MOTOR_A2_PIN, OUTPUT);
+  pinMode(MOTOR_B2_PIN, OUTPUT);
+
+  pinMode(PWM_MOTOR_1, OUTPUT);
+  pinMode(PWM_MOTOR_2, OUTPUT);
+
+  pinMode(CURRENT_SEN_1, OUTPUT);
+  pinMode(CURRENT_SEN_2, OUTPUT);  
+
+  pinMode(EN_PIN_1, OUTPUT);
+  pinMode(EN_PIN_2, OUTPUT);
+
+  pinMode(CHANNEL1, INPUT);
+  pinMode(CHANNEL2, INPUT);
+  pinMode(CHANNEL3, INPUT);
+  pinMode(CHANNEL4, INPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+    digitalWrite(EN_PIN_1, HIGH);
+    digitalWrite(EN_PIN_2, HIGH);
+    
+    channelData[0] = pulseIn(CHANNEL1, HIGH);//left-right channel
+    channelData[1] = pulseIn(CHANNEL2, HIGH);//forward backward channel
+    channelData[2] = pulseIn(CHANNEL3, HIGH);
+    channelData[3] = pulseIn(CHANNEL4, HIGH);
+    
+    if(channelData[1] >= CHANNEL_NEUTRAL_LOW && channelData[1] <= CHANNEL_NEUTRAL_HIGH) Stop();
+    if(channelData[1] > CHANNEL_NEUTRAL_HIGH ) Forward();
+    if(channelData[1] < CHANNEL_NEUTRAL_LOW && channelData[1] > CHANNEL_ONE_TWO_LOW) Reverse(); 
+}
+
+void Stop()
+{
+  Serial.println("Stop");
+  usMotor_Status = BRAKE;
+  motorGo(MOTOR_1, usMotor_Status, 0);
+  motorGo(MOTOR_2, usMotor_Status, 0);
+}
+
+void Forward()
+{
+  Serial.println("Forward");
+  usMotor_Status = CW;
+  motorGo(MOTOR_1, usMotor_Status, usSpeed);
+  motorGo(MOTOR_2, usMotor_Status, usSpeed);
+}
+
+void Reverse()
+{
+  Serial.println("Reverse");
+  usMotor_Status = CCW;
+  motorGo(MOTOR_1, usMotor_Status, usSpeed);
+  motorGo(MOTOR_2, usMotor_Status, usSpeed);
+}
+
+void IncreaseSpeed()
+{
+  usSpeed = usSpeed + 10;
+  if(usSpeed > 255)
+  {
+    usSpeed = 255;  
+  }
+  
+  Serial.print("Speed +: ");
+  Serial.println(usSpeed);
+
+  motorGo(MOTOR_1, usMotor_Status, usSpeed);
+  motorGo(MOTOR_2, usMotor_Status, usSpeed);  
+}
+
+void DecreaseSpeed()
+{
+  usSpeed = usSpeed - 10;
+  if(usSpeed < 0)
+  {
+    usSpeed = 0;  
+  }
+  
+  Serial.print("Speed -: ");
+  Serial.println(usSpeed);
+
+  motorGo(MOTOR_1, usMotor_Status, usSpeed);
+  motorGo(MOTOR_2, usMotor_Status, usSpeed);  
+}
+
+void motorGo(uint8_t motor, uint8_t direct, uint8_t pwm)         //Function that controls the variables: motor(0 ou 1), direction (cw ou ccw) e pwm (entra 0 e 255);
+{
+  if(motor == MOTOR_1)
+  {
+    if(direct == CW)
+    {
+      digitalWrite(MOTOR_A1_PIN, LOW); 
+      digitalWrite(MOTOR_B1_PIN, HIGH);
+    }
+    else if(direct == CCW)
+    {
+      digitalWrite(MOTOR_A1_PIN, HIGH);
+      digitalWrite(MOTOR_B1_PIN, LOW);      
+    }
+    else
+    {
+      digitalWrite(MOTOR_A1_PIN, LOW);
+      digitalWrite(MOTOR_B1_PIN, LOW);            
+    }
+    
+    analogWrite(PWM_MOTOR_1, pwm); 
+  }
+  else if(motor == MOTOR_2)
+  {
+    if(direct == CW)
+    {
+      digitalWrite(MOTOR_A2_PIN, LOW);
+      digitalWrite(MOTOR_B2_PIN, HIGH);
+    }
+    else if(direct == CCW)
+    {
+      digitalWrite(MOTOR_A2_PIN, HIGH);
+      digitalWrite(MOTOR_B2_PIN, LOW);      
+    }
+    else
+    {
+      digitalWrite(MOTOR_A2_PIN, LOW);
+      digitalWrite(MOTOR_B2_PIN, LOW);            
+    }
+    
+    analogWrite(PWM_MOTOR_2, pwm);
+  }
+}
