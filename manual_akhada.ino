c957e3867f76c429c885132ddb3da5bbcba7ea65
codedump00 <blazinasis@gmail.com>
2019-02-16 14:04:19 +0545

    added slight fixes. added comments

--- manual_akhada.ino
+++ manual_akhada.ino
@@ -34,14 +34,24 @@
 
 
 //Channel pulse read parameters
-#define CHANNEL_NEUTRAL_LOW 1400
-#define CHANNEL_NEUTRAL_HIGH 1550
+//Below CHANNEL_NEUTRAL_LOW robot moves backward.
+#define CHANNEL_NEUTRAL_LOW 1400 //Lower limit to remain in stop state.
+#define CHANNEL_NEUTRAL_HIGH 1550//Upper limit to remain in stop state.
+//Above CHANNEL_NEUTRAL_HIGH robot moves forward.
+
+//This parameter are not needed.
+//1960 is the highest reading from channel-1 and channel-2.
 #define CHANNEL_ONE_TWO_HIGH  1960
+//990 is the lowest.
+//During startup reading is 0 from each channel.
+//This acts as a parameter to check if the robot should be moving backwards.
+//The robot should not move backwards when value is less than 990 i.e 0
 #define CHANNEL_ONE_TWO_LOW  990
 
 short usSpeed = 150;  //default motor speed
 unsigned short usMotor_Status = BRAKE;
 //Array to store channel readings.
+//Stores upto CHANNEL-1 to CHANNEL-4
 int channelData[4];
 
 void setup()                         
@@ -61,8 +71,12 @@ void setup()
   pinMode(EN_PIN_1, OUTPUT);
   pinMode(EN_PIN_2, OUTPUT);
 
+ //for left right movement
   pinMode(CHANNEL1, INPUT);
+  //for forward-Reverse movement
   pinMode(CHANNEL2, INPUT);
+
+  //Not used right now.
   pinMode(CHANNEL3, INPUT);
   pinMode(CHANNEL4, INPUT);
 
@@ -74,14 +88,48 @@ void loop()
     digitalWrite(EN_PIN_1, HIGH);
     digitalWrite(EN_PIN_2, HIGH);
     
+    //This array element holds the reading of channnel-1.
+    //Channel-1 is controlled by the left-right movement in the joy-stick.
     channelData[0] = pulseIn(CHANNEL1, HIGH);//left-right channel
+
+    //This array element holds the reading of channel-2.
+    //Channel-2 is controlled by the forward-backward movement in the joy-stick.
     channelData[1] = pulseIn(CHANNEL2, HIGH);//forward backward channel
+    //Not used right now.
+    //Couldn't calibrate,
     channelData[2] = pulseIn(CHANNEL3, HIGH);
     channelData[3] = pulseIn(CHANNEL4, HIGH);
+    //****************************************//
     
-    if(channelData[1] >= CHANNEL_NEUTRAL_LOW && channelData[1] <= CHANNEL_NEUTRAL_HIGH) Stop();
-    if(channelData[1] > CHANNEL_NEUTRAL_HIGH ) Forward();
-    if(channelData[1] < CHANNEL_NEUTRAL_LOW && channelData[1] > CHANNEL_ONE_TWO_LOW) Reverse(); 
+    //Remain stopped while the value is between 1400, 1550
+    if(channelData[1] >= CHANNEL_NEUTRAL_LOW && channelData[1] <= CHANNEL_NEUTRAL_HIGH){ 
+      Stop();
+    
+    }
+    //If the value is greater than 1550 move forward.
+    if(channelData[1] > CHANNEL_NEUTRAL_HIGH ){
+       Forward();
+    }
+
+    //If the value is less than 1400 move backward.
+    if(channelData[1] < CHANNEL_NEUTRAL_LOW && channelData[1] > CHANNEL_ONE_TWO_LOW){
+       Reverse();
+    }
+
+    if(channelData[0] >= CHANNEL_NEUTRAL_LOW && channelData[1] <= CHANNEL_NEUTRAL_HIGH){
+       Stop();
+    }
+
+    //If channel 1 is greater than 1550 move right.
+    if(channelData[0] > CHANNEL_NEUTRAL_HIGH ){
+       Right();
+    }
+    //If channel 1 is less than 1400 move left.
+    //This might need to be fixed for left right movement.
+    //By changing functions called i.e. Left() or Right()
+    if(channelData[0] < CHANNEL_NEUTRAL_LOW && channelData[1] > CHANNEL_ONE_TWO_LOW){
+       Left();
+    } 
 }
 
 void Stop()
